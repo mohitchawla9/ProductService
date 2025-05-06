0a0afa07d69b5143ddbef4a7bff1d0647f53c114
Chawla, Mohit <Mohit.Chawla@united.com>
2025-05-06 12:07:48 +0530

    Exceptional Handler

--- src/main/java/com/productservice/productservice/controllers/ProductController.java
+++ src/main/java/com/productservice/productservice/controllers/ProductController.java
@@ -1,10 +1,14 @@
 package com.productservice.productservice.controllers;
 
+import com.productservice.productservice.dtos.ExceptionDto;
 import com.productservice.productservice.dtos.FakeStoreProductDto;
 import com.productservice.productservice.dtos.GenericProductDto;
+import com.productservice.productservice.exceptions.ProductNotFoundException;
 import com.productservice.productservice.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,7 +26,7 @@ public class ProductController {
    }
 
     @GetMapping("/{id}")
-    public GenericProductDto getProductById(@PathVariable("id") Long id) {
+    public GenericProductDto getProductById(@PathVariable("id") Long id) throws ProductNotFoundException {
         //return "Product fetched with id: " + id;
         return productService.getProductById(id);
     }
@@ -44,6 +48,15 @@ public class ProductController {
     public void updateProductById(@RequestBody GenericProductDto genericProductDto, @PathVariable("id") Long id) {
        productService.updateProductById(id, genericProductDto);
     }
+
+//    @ExceptionHandler(ProductNotFoundException.class)
+//    private ResponseEntity<ExceptionDto> handleProductNotFoundException(ProductNotFoundException productNotFoundException) {
+//        ExceptionDto exceptionDto = new ExceptionDto();
+//        exceptionDto.setMessage(productNotFoundException.getMessage());
+//        exceptionDto.setHttpStatus(HttpStatus.NOT_FOUND);
+//       return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+//    }
+
 }
 
 /*
--- src/main/java/com/productservice/productservice/services/ProductService.java
+++ src/main/java/com/productservice/productservice/services/ProductService.java
@@ -1,12 +1,13 @@
 package com.productservice.productservice.services;
 
 import com.productservice.productservice.dtos.GenericProductDto;
+import com.productservice.productservice.exceptions.ProductNotFoundException;
 
 import java.util.List;
 
 public interface ProductService {
 
-    GenericProductDto getProductById(Long id);
+    GenericProductDto getProductById(Long id) throws ProductNotFoundException;
     List<GenericProductDto> getAllProducts();
     GenericProductDto deleteProductById(Long id);
     GenericProductDto createProduct(GenericProductDto genericProductDto);
