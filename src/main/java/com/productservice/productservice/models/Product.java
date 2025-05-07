2d919dc18f3918f036f47579dd250cf429cb90a0
Chawla, Mohit <Mohit.Chawla@united.com>
2025-05-07 13:41:29 +0530

    DB Connectivity

--- src/main/java/com/productservice/productservice/models/Product.java
+++ src/main/java/com/productservice/productservice/models/Product.java
@@ -1,14 +1,21 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
     private String title;
-    private String desc;
+    private String description;
     private int price;
     private String image;
+
+    //Category isn't primitive attribute,it is a relation
+    @ManyToOne
     private Category category;
 }
