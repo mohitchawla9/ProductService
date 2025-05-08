1e09eb082a08da956eb2e918e61874fbb523e916
Chawla, Mohit <Mohit.Chawla@united.com>
2025-05-08 17:15:42 +0530

    Understanding Cardinalities

--- src/main/java/com/productservice/productservice/ProductServiceApplication.java
+++ src/main/java/com/productservice/productservice/ProductServiceApplication.java
@@ -6,6 +6,10 @@ import com.productservice.productservice.inheritancerelations.singletable.Studen
 import com.productservice.productservice.inheritancerelations.singletable.User;
 import com.productservice.productservice.inheritancerelations.singletable.StudentRepository;
 import com.productservice.productservice.inheritancerelations.singletable.UserRepository;
+import com.productservice.productservice.models.Category;
+import com.productservice.productservice.models.Product;
+import com.productservice.productservice.repositories.CategoryRepository;
+import com.productservice.productservice.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,18 +17,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProductServiceApplication implements CommandLineRunner {
+	private final CategoryRepository categoryRepository;
+	private final ProductRepository productRepository;
 
-	private MentorRepository mentorRepository;
-	private UserRepository userRepository;
-	private StudentRepository studentRepository;
-
-	ProductServiceApplication(@Qualifier("st_mentorrepo") MentorRepository mentorRepository,
-							  @Qualifier("st_userrepo") UserRepository userRepository,
-							  @Qualifier("st_studentrepo") StudentRepository studentRepository) {
-		this.mentorRepository = mentorRepository;
-		this.userRepository = userRepository;
-		this.studentRepository = studentRepository;
+	public ProductServiceApplication(CategoryRepository categoryRepository, ProductRepository productRepository) {
+		this.categoryRepository = categoryRepository;
+		this.productRepository = productRepository;
 	}
+
+//	private MentorRepository mentorRepository;
+//	private UserRepository userRepository;
+//	private StudentRepository studentRepository;
+//
+//	ProductServiceApplication(@Qualifier("st_mentorrepo") MentorRepository mentorRepository,
+//							  @Qualifier("st_userrepo") UserRepository userRepository,
+//							  @Qualifier("st_studentrepo") StudentRepository studentRepository) {
+//		this.mentorRepository = mentorRepository;
+//		this.userRepository = userRepository;
+//		this.studentRepository = studentRepository;
+//	}
 
 //	private MentorRepository mentorRepository;
 //	private StudentRepository studentRepository;
@@ -67,22 +78,34 @@ public class ProductServiceApplication implements CommandLineRunner {
 //			System.out.println(user1.toString());
 //		}
 
-		User user = new User();
-		user.setName("Arshi");
-		user.setEmail("arshi@gmail.com");
-		userRepository.save(user);
+//		User user = new User();
+//		user.setName("Arshi");
+//		user.setEmail("arshi@gmail.com");
+//		userRepository.save(user);
+//
+//		Mentor mentor = new Mentor();
+//		mentor.setName("Deepak");
+//		mentor.setEmail("deepak@gmail.com");
+//		mentor.setAvgRating(4.7);
+//		mentorRepository.save(mentor);
+//
+//		Student student = new Student();
+//		student.setName("harsh");
+//		student.setEmail("harsh@gmail.com");
+//		student.setPsp(99.8);
+//		studentRepository.save(student);
 
-		Mentor mentor = new Mentor();
-		mentor.setName("Deepak");
-		mentor.setEmail("deepak@gmail.com");
-		mentor.setAvgRating(4.7);
-		mentorRepository.save(mentor);
+		Category category = new Category();
+		category.setName("Apple Devices");
 
-		Student student = new Student();
-		student.setName("harsh");
-		student.setEmail("harsh@gmail.com");
-		student.setPsp(99.8);
-		studentRepository.save(student);
+		Category savedCategory =  categoryRepository.save(category);
+
+		Product product = new Product();
+		product.setTitle("iphone 15 pro");
+		product.setCategory(savedCategory);
+		product.setDescription("Best iphone ever");
+
+		Product savedProduct = productRepository.save(product);
 
 
 	}
--- src/main/java/com/productservice/productservice/models/Category.java
+++ src/main/java/com/productservice/productservice/models/Category.java
@@ -2,12 +2,18 @@ package com.productservice.productservice.models;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 public class Category extends BaseModel{
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
