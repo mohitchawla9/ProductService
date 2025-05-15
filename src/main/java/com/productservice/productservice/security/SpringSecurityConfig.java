c002ea307a95dabe98c2a96301fbad3e83c71e28
Chawla, Mohit <Mohit.Chawla@united.com>
2025-05-15 13:57:01 +0530

    Integrating Product and User Service

---
+++ src/main/java/com/productservice/productservice/security/SpringSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.productservice.productservice.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        //.requestMatchers("/products").hasAuthority("admin")
+                        //.anyRequest().authenticated()
+                        //.requestMatchers("/getProducts").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .httpBasic(withDefaults());
+//                .csrf().disable()
+//                .cors().disable()
+                // Form login handles the redirect to the login page from the
+                // authorization server filter chain
+//                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
